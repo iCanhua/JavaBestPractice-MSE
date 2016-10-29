b09b187b0b365361628113efce2e5ac3453b7575
iCanhua <525921307@qq.com>
2016-10-30 00:41:33 +0800

    update the B extends A

--- src/main/java/com/scut/fan/testlearn/classloadprocess/A.java
+++ src/main/java/com/scut/fan/testlearn/classloadprocess/A.java
@@ -4,14 +4,15 @@ package com.scut.fan.testlearn.classloadprocess;
  * Created by FAN on 2016/10/27.
  */
 public class A {
+
     static {
-        System.out.println("A");
+        System.out.println("类A的静态代码快");
     }
     {
-        System.out.println("Ia");
+        System.out.println("类A的非静态代码块");
     }
     A(){
-        System.out.println("a");
+        System.out.println("A被实例化了");
     }
 }
 
